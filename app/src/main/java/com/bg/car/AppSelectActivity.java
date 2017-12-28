919c3bcc982cabc42c072180548a20b80c31af7b
BinGe1987 <shenyaobin@foxmail.com>
2017-12-28 20:59:03 +0800

    code

--- app/src/main/java/com/bg/car/AppSelectActivity.java
+++ app/src/main/java/com/bg/car/AppSelectActivity.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.ResolveInfo;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -17,6 +17,8 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bg.library.UI.Dialog.Progress;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,37 +28,70 @@ import java.util.List;
 
 public class AppSelectActivity extends AppCompatActivity {
 
+    private Handler mHandler;
+    private static List<PackageInfo> mApps = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_select);
+        mHandler = new Handler();
+        Progress.show(this);
 
-        final List<PackageInfo> appList = new ArrayList<>();
-        try {
-            List<PackageInfo> infos = getPackageManager().getInstalledPackages(0);
-            for (int i = 0; i < infos.size(); i++) {
-                PackageInfo packageInfo = infos.get(i);
-                if (packageInfo.applicationInfo.loadIcon(getPackageManager()) == null) {
-                    continue;
+        if (mApps.size() > 0) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    initApps();
+                    Progress.dismiss();
                 }
-                Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
-                resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-                resolveIntent.setPackage(packageInfo.packageName);
-                List<ResolveInfo> list = getPackageManager()
-                        .queryIntentActivities(resolveIntent, 0);
-                if (list.size() == 0) {
-                    continue;
-                }
-                appList.add(packageInfo);
-            }
-        } catch (Exception e) {
+            }, 10);
+            return;
         }
+
+        new Thread() {
+            public void run() {
+                final List<PackageInfo> appList = new ArrayList<>();
+                try {
+                    List<PackageInfo> infos = getPackageManager().getInstalledPackages(0);
+                    for (int i = 0; i < infos.size(); i++) {
+                        PackageInfo packageInfo = infos.get(i);
+                        if (packageInfo.applicationInfo.loadIcon(getPackageManager()) == null) {
+                            continue;
+                        }
+                        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+                        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+                        resolveIntent.setPackage(packageInfo.packageName);
+                        List<ResolveInfo> list = getPackageManager()
+                                .queryIntentActivities(resolveIntent, 0);
+                        if (list.size() == 0) {
+                            continue;
+                        }
+                        appList.add(packageInfo);
+                    }
+                } catch (Exception e) {
+                }
+                mApps.clear();
+                mApps.addAll(appList);
+
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        initApps();
+                        Progress.dismiss();
+                    }
+                }, 10);
+            }
+        }.start();
+    }
+
+    private void initApps() {
 
         GridView gv = (GridView) findViewById(R.id.apps);
         gv.setAdapter(new BaseAdapter() {
             @Override
             public int getCount() {
-                return appList.size();
+                return mApps.size();
             }
 
             @Override
@@ -74,7 +109,7 @@ public class AppSelectActivity extends AppCompatActivity {
                 if (view == null) {
                     view = LayoutInflater.from(AppSelectActivity.this).inflate(R.layout.app_item, null);
                 }
-                PackageInfo info = appList.get(i);
+                PackageInfo info = mApps.get(i);
                 ImageView iv = view.findViewById(R.id.image);
                 iv.setImageDrawable(info.applicationInfo.loadIcon(getPackageManager()));
                 TextView tv = view.findViewById(R.id.text);
@@ -85,7 +120,7 @@ public class AppSelectActivity extends AppCompatActivity {
         gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                PackageInfo info = appList.get(i);
+                PackageInfo info = mApps.get(i);
                 String packageName = info.packageName;
                 int id = getIntent().getIntExtra("id", 0);
                 if (id > 0) {
@@ -100,7 +135,6 @@ public class AppSelectActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
 
 }
